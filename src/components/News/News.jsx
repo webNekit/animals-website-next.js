c929d7105bafb19b87b1eaed78b78955fa8f481b
webNekit <mochalov-hunter@mail.ru>
2024-03-30 00:15:25 +0300

    продолжаю делать раздел с новостями

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -6,6 +6,7 @@ const News = () => {
       <div className="container mx-auto">
         <div className="w-full">
           <h2 className="section-title">важные новости</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5"></div>
         </div>
       </div>
     </section>
