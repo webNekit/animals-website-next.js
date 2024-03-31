3c58c608534b49fbc0470d573ca445ba9186929b
webNekit <mochalov-hunter@mail.ru>
2024-03-31 14:25:39 +0300

    Начал делать страницу реквизиты

--- src/app/(route)/requisites/page.jsx
+++ src/app/(route)/requisites/page.jsx
@@ -5,6 +5,11 @@ export default function requisites() {
   return (
     <>
         <HeaderPage introTitle={'Реквизиты'} />
+        <section className="w-full py-12">
+          <div className="container mx-auto">
+            <h2 className="inline-block py-1 text-left text-2xl font-medium border-b-2 border-primary">Перевод на карту</h2>
+          </div>
+        </section>
     </>
   )
 }
