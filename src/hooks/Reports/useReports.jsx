07c16b88df867798a8402501909ec7b7db7ed9eb
webNekit <mochalov-hunter@mail.ru>
2024-04-07 20:10:10 +0300

    Пытался передать отчеты

---
+++ src/hooks/Reports/useReports.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { axiosClient } from '@/utils/GlobalApi';
+import React, { useEffect, useState } from 'react';
+
+const useReports = () => {
+
+    const [ reportsData, setReportsData ] = useState([]);
+
+    useEffect(() => {
+        getReports();
+    }, [])
+
+    const getReports = () => {
+        axiosClient.get("/reports?sort[0]=id:desc&populate=*").then(resp => {
+            console.log(resp.data.data);
+            setReportsData(resp.data.data);
+        }).catch(error => {
+            console.log("Ошибка получения данных", error)
+        });
+    };
+
+    return reportsData;
+}
+
+export default useReports;
